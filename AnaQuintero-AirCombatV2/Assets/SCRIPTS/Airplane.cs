86c3f8ce993850ee5d6a50f09a18caef5371b97c
Ana Quintero <ana.quintero.vanegas@gmail.com>
2018-04-17 21:42:29 -0500

    Parcial 2 AirBattle

---
+++ AnaQuintero-AirCombatV2/Assets/SCRIPTS/Airplane.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airplane : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Mover hacia adelante
+	 {
+			this.transform.Translate (0.0f, 0.0f, 4.00f);
+		}
+		//movemos el avion hacia arriba
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			this.transform.Translate (0.0f, 7.00f, 0.0f);
+		}
+
+		// Mover hacia abajo
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			this.transform.Translate (0.0f, -7.00f, 0.0f);
+		}
+
+		// Mover hacia el lado derecha
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			this.transform.Translate (7.00f, 0.0f, 0.0f);
+		}
+
+		// Mover hacia el lado izquierda
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			this.transform.Translate (-7.00f, 0.0f, 0.0f);
+		}
+		// Si la tecla espacio fue presionada
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			// Reconstruyo un gameObject a partir
+			// de la informacion guardada en un prefab
+
+			GameObject Missile = GameObject.Instantiate (Resources.Load ("Prefabs/Missile") as GameObject);
+
+			// Hago que el misil sea hijo del avion
+
+			Missile.transform.SetParent (this.gameObject.transform);
+
+			// Muevo el misil a unas coordenadas relativas con respecto al avion
+			Missile.transform.localPosition = new Vector3(-10.0f, 21.8f, 7.0f);
+
+			//Saco al misil del avion
+
+			Missile.transform.SetParent (null);
+		}
+		
+	}
+}
---
+++ AnaQuintero-AirCombatV2/Assets/SCRIPTS/MISSILE.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MISSILE : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+		Destroy (this.gameObject,8);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// mover hacia delante
+		this.transform.Translate (0.0f, 10.0f, 0.0f );
+		
+	}
+
+	void OnCollisionEnter(Collision collision) {
+
+		// = Asignar
+		//==Preguntar
+
+		if (collision.collider.gameObject.tag == "MarsAlien") {
+
+
+		// Carga el prefab de una explosion Mars
+
+			GameObject MarsExplosion = GameObject.Instantiate(Resources.Load ("Prefabs/MarsExplosion") as GameObject);
+			
+			//Muevo la explisii=on a la posicion donde esta la nave
+
+			MarsExplosion.transform.position = collision.collider.gameObject.transform.position;
+
+		// Cuando el misil colision con la plataforma
+		// La elimina
+
+		Destroy (collision.collider.gameObject);
+
+		//Destruir el misil
+
+		Destroy (this.gameObject);
+		}
+
+		if (collision.collider.gameObject.tag == "JupiterAlien") {
+
+			// Carga el prefab de una explosion Mars
+
+			GameObject JupiterExplosion = GameObject.Instantiate(Resources.Load ("Prefabs/JupiterExplosion") as GameObject);
+
+			//Muevo la explisii=on a la posicion donde esta la nave
+
+			JupiterExplosion.transform.position = collision.collider.gameObject.transform.position;
+
+			// Cuando el misil colision con la plataforma
+			// La elimina
+
+			Destroy (collision.collider.gameObject);
+
+			//Destruir el misil
+
+			Destroy (this.gameObject);
+	
+		}
+	
+	}
+}
